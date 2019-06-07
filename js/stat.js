2ffdd388e082323f50135c9e480a5d9c36659cb0
Kristine <magic.rooter@gmail.com>
2019-06-07 14:14:59 +0300

    Выполнила 2 задание. Исправила ошибки линтера

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
- 'use strict';
+'use strict';
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
@@ -6,20 +6,15 @@ var CLOUD_X = 150;
 var CLOUD_Y = 50;
 var GAP = 50;
 var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
-var COLOR_ME = 'rgba(255, 0, 0, 1)';
-var COLOR_PLAYERS = 'rgba(0, 0, 255)';
-var leftColumn = 150;
 
-
- var showCloud = function (ctx, x, y, color) {
+var showCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
 
   showCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   showCloud(ctx, 100, 10, '#ffffff');
@@ -28,45 +23,34 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
     }
-  }
 
-  return maxElement;
-};
+    return maxElement;
+  };
 
-var maxTime = getMaxElement(times);
+  var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-  var x = CLOUD_X + (GAP + BAR_WIDTH) * i;
-  var y = CLOUD_Y + GAP + BAR_HEIGHT;
+    var x = CLOUD_X + (GAP + BAR_WIDTH) * i;
+    var y = CLOUD_Y + GAP + BAR_HEIGHT;
+    var barHeight = BAR_HEIGHT * (times[i] / maxTime) * -1;
 
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], x, y + FONT_GAP);
 
-  var barHeight = BAR_HEIGHT * (times[i] / maxTime) * -1;
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'red';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    }
 
-  ctx.fillStyle = '#000000';
-   ctx.fillText(names[i], x, y + FONT_GAP);
-
-
-
-  if (names[i] == 'Вы') {
-    ctx.fillStyle = 'red';
+    ctx.fillRect(x, y, BAR_WIDTH, barHeight);
   }
-
-  else {
-    ctx.fillStyle = "rgba(0, 0, 255, " + Math.random() + ")";
-  }
-
-
-
-
-   ctx.fillRect(x, y, BAR_WIDTH, barHeight);
-  }
-
-
 };
